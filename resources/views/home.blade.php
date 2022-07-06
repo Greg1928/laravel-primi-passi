daaa9c5702dab8f573afb2f637333329673314ad
Greg1928 <gregoriosortino@protonmail.com>
2022-07-06 16:28:36 +0200

    add custom home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel Primi Passi</title>
+</head>
+<style>
+    h1{
+        text-align: center;
+        font-size: 50px;
+        color: white;
+        text-decoration: underline;
+    }
+    body{
+        background-color: darkgoldenrod;
+    }
+</style>
+<body>
+    <header>
+        <h1>Welcome!</h1>
+    </header>
+</body>
+</html>
